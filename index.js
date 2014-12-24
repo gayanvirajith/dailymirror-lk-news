0ec806a69354ffa965a15a6fdb08262fbdd65724
Gayan Virajith <gayanvirajith@gmail.com>
2014-12-24 11:45:04 +0530

    Update feed URL(s)

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 var createMenu = require('terminal-menu');
 
+/*
+ * Menu map with the {title} - {url}
+ */
+var menuMap = {
+  "breaking_news": "http://www.dailymirror.lk/RSS_Feeds/breaking-news",
+  "world_news": "http://www.dailymirror.lk/RSS_Feeds/world-news-main",
+  "business": "http://www.dailymirror.lk/RSS_Feeds/business-main",
+  "opinion": "http://www.dailymirror.lk/RSS_Feeds/opinion",
+  "sports": "http://sports.dailymirror.lk/rss",
+  "video": "http://www.dailymirror.lk/RSS_Feeds/videos",
+  "cartoon": "http://www.dailymirror.lk/RSS_Feeds/cartoon-of-the-day",
+  "travel": "http://www.dailymirror.lk/RSS_Feeds/travel-main",
+  "technology": "http://www.dailymirror.lk/RSS_Feeds/technology"
+}
+
 var menu = createMenu({ width: 29, x: 4, y: 2 });
 menu.reset();
 menu.write('Dailymirror.lk News Terminal\n');
 menu.write('-------------------------\n');
 
-menu.add('Breaking News');
-menu.add('World News');
-menu.add('Business');
-menu.add('Opinion');
-menu.add('Sports');
-menu.add('Video');
-menu.add('Cartoon');
-menu.add('Travel');
-menu.add('Technology');
+for (var k in menuMap) {
+  $menu_title = k.replace('_', " ")
+  menu.add($menu_title.toUpperCase());
+}
+
 menu.add('EXIT');
 
 menu.on('select', function (label) {
     menu.close();
-    console.log('SELECTED: ' + label);
+    $selectedItem = label.replace(" ", "_").toLowerCase();
+    console.log(menuMap[$selectedItem]);
 });
+
 menu.createStream().pipe(process.stdout);
